a23fa85403f155b1108a68ed435bbea5883eaace
Luis Alaniz Terán <laat.elt@gmail.com>
2025-05-14 00:33:23 -0400

    Si se pudo1

--- components/WhatsAppButton.js
+++ components/WhatsAppButton.js
@@ -5,7 +5,7 @@ import styles from '../styles/WhatsAppButton.module.css';
 
 const WhatsAppButton = () => {
   const phoneNumber = '59177894875';
-  const message = 'Hola, me gustaría obtener más información sobre Elektron Lab';
+  const message = '¡Hola! Podrías darme más información de...';
 
   const handleClick = () => {
     const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
